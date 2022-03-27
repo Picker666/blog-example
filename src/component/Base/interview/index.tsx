5148e27a02f053b687f874f7b0bfe648cdd1491b
Picker666 <1034790395@qq.com>
2022-03-27 21:29:55 +0800

    note for and forEach example

--- src/component/Base/interview/index.tsx
+++ src/component/Base/interview/index.tsx
@@ -23,12 +23,53 @@ const InterviewIndex = () => {
     console.log(a, '====', b);
   };
 
+  const forAndForEach = (arrLength: number) => {
+    let arr = new Array(arrLength);
+
+    console.time(`arrLength=${arrLength} for`);
+    for (let i = 0; i < arrLength; i++) {}
+    console.timeEnd(`arrLength=${arrLength} for`);
+
+    console.time(`arrLength=${arrLength} forEach`);
+    arr.forEach((a) => {});
+    console.timeEnd(`arrLength=${arrLength} forEach`);
+  };
+
   return (
     <div>
       <h1>this is interview index page</h1>
       <button onClick={objWithArray}>object With Array</button>
       <br />
       <button onClick={OperationPriority}>Operation Priority</button>
+      <br />
+      <button
+        onClick={() => {
+          forAndForEach(100000);
+        }}
+      >
+        for And ForEach 100000
+      </button>
+      <button
+        onClick={() => {
+          forAndForEach(1000000);
+        }}
+      >
+        for And ForEach 1000000
+      </button>
+      <button
+        onClick={() => {
+          forAndForEach(10000000);
+        }}
+      >
+        for And ForEach 10000000
+      </button>
+      <button
+        onClick={() => {
+          forAndForEach(100000000);
+        }}
+      >
+        for And ForEach 100000000
+      </button>
       <br />
     </div>
   );
