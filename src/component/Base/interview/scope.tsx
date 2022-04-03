6ae6d144347907ecd6a8865cc00c4d3551b78ca5
Picker666 <1034790395@qq.com>
2022-04-03 14:03:23 +0800

    note scope example

--- src/component/Base/interview/scope.tsx
+++ src/component/Base/interview/scope.tsx
@@ -55,6 +55,63 @@ const Index = () => {
     // console.log('a' ++ 'b');
   }
 
+  function scopeA(b) {
+    console.log(b, '===a===');
+    function b() {
+      console.log(b);
+    }
+    b();
+    b = 1;
+
+    console.log(b, '=====last=====');
+  }
+
+  function scopeT3(greet) {
+    console.log(greet); //?
+    var greet = 'hello';
+    console.log(greet); //?
+    function greet() {}
+    console.log(greet); //?
+  }
+
+  function scopeTest(a, b) {
+    console.log(a);
+    console.log(b);
+    var b = 234;
+    console.log(b);
+    a = 123;
+    console.log(a);
+    function a() {}
+    var a;
+    b = 456;
+    console.log(b);
+    var b = function () {};
+    console.log(a);
+    console.log(b);
+  }
+
+  function scopeTest2() {
+    a = 100;
+    function demo(e) {
+      function e() {}
+      arguments[0] = 2;
+      console.log(e); //  e() {}
+      if (a) {
+        var b = 123;
+      }
+      a = 10;
+      var a;
+      console.log(b); // 123
+      f = 123;
+      console.log(a); // 10
+    }
+
+    // var a;
+    demo(1);
+    console.log(a); // 10
+    console.log(f); // 123
+  }
+
   return (
     <div>
       <h1>this is interview scope page</h1>
@@ -65,6 +122,14 @@ const Index = () => {
       <button onClick={prototypeAndInstance}>prototype And Instance</button>
       <br />
       <button onClick={handleOperator}>handle Operator</button>
+      <br />
+      <button onClick={() => scopeA(6)}>scope A</button>
+      <br />
+      <button onClick={() => scopeT3(null)}>scope t3</button>
+      <br />
+      <button onClick={() => scopeTest(1, 2)}>scope test</button>
+      <br />
+      <button onClick={scopeTest2}>scope test2</button>
       <br />
     </div>
   );
