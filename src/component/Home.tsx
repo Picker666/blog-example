cb4ad91fe21bcc7b1a0d5490ac6de1509fe5e724
Picker666 <1034790395@qq.com>
2022-04-24 22:14:46 +0800

    note grid

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,5 +1,54 @@
-const Home = () => {
-  return <div>this is home page</div>
-}
+import { useEffect, useMemo, useState } from 'react';
+// import {}
+import './index.less';
 
-export default Home
+const Home = () => {
+  const [goodsList, setGoodsList] = useState([]);
+  useEffect(() => {
+    const goods = [
+      {
+        url: '',
+        name: '苹果',
+        price: 10.99,
+      },
+      {
+        url: '',
+        name: '香蕉',
+        price: 20.99,
+      },
+      {
+        url: '',
+        name: '菠萝-广西香蕉新鲜包熟不熟不要钱666',
+        price: 30.99,
+      },
+      {
+        url: '',
+        name: '车厘子',
+        price: 40.99,
+      },
+      {
+        url: '',
+        name: '西梅',
+        price: 50.99,
+      },
+    ];
+  }, []);
+
+  const goodsDomsList = useMemo(() => {
+    return goodsList;
+  }, [goodsList]);
+
+  return (
+    <div className="container">
+      <div className="good">A</div>
+      <div className="good">B</div>
+      <div className="good">C</div>
+      <div className="good">D</div>
+      <div className="good">E</div>
+      {/* <h1>this is home page</h1>
+      <div>{goodsDomsList}</div> */}
+    </div>
+  );
+};
+
+export default Home;
