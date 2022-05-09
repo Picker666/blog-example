0d4259ed42d4ee974e7998cebc7d28387d7100ad
Picker666 <1034790395@qq.com>
2022-05-09 09:26:25 +0800

    note flex decimal

--- src/component/Base/Flex.tsx
+++ src/component/Base/Flex.tsx
@@ -199,6 +199,94 @@ const Flex = () => {
           <div>黄框：content为 400px</div>
         </div>
       </div>
+
+      <div className="flexGrowDecimal">
+        <h2>10、flex: x， x之和大于等于1</h2>
+        <div className="flexContainer">
+          <div className="flexLeft">
+            width: 300px;
+            <br />
+            flex: 0.6 2 200px;
+            <br />
+            拉伸后的宽度：260px
+          </div>
+          <div className="flexRight">
+            width: 200px;
+            <br />
+            flex: 0.9 3 300px;
+            <br />
+            拉伸后的宽度：340px
+          </div>
+        </div>
+        <div className="instruction">
+          <div>黄框：content为 600px</div>
+        </div>
+
+        <h2>11、flex 为x, x之和小于1</h2>
+        <div className="flexContainer1">
+          <div className="flexLeft">
+            width: 300px;
+            <br />
+            flex: 0.2 2 200px;
+            <br />
+            收缩后的宽度：220px
+          </div>
+          <div className="flexRight">
+            width: 200px;
+            <br />
+            flex: 0.3 3 300px;;
+            <br />
+            收缩后的宽度：330px；
+          </div>
+        </div>
+        <div className="instruction">
+          <div>黄框：content为 600px</div>
+        </div>
+      </div>
+
+      <div className="flexShrinkDecimal">
+        <h2>12、flex: 1 x npx， x之和大于等于1</h2>
+        <div className="flexContainer">
+          <div className="flexLeft">
+            width: 300px;
+            <br />
+            flex: 1 0.6 200px;
+            <br />
+            收缩后的宽度：169.23px
+          </div>
+          <div className="flexRight">
+            width: 200px;
+            <br />
+            flex: 1 0.9 300px;
+            <br />
+            收缩后的宽度：230.77px
+          </div>
+        </div>
+        <div className="instruction">
+          <div>黄框：content为 400px</div>
+        </div>
+
+        <h2>13、flex: 1 x npx, x之和小于1</h2>
+        <div className="flexContainer1">
+          <div className="flexLeft">
+            width: 300px;
+            <br />
+            flex: 2 0.2 200px;
+            <br />
+            收缩后的宽度：184.61px
+          </div>
+          <div className="flexRight">
+            width: 200px;
+            <br />
+            flex: 3 0.3 300px;;
+            <br />
+            收缩后的宽度：265.39px；
+          </div>
+        </div>
+        <div className="instruction">
+          <div>黄框：content为 400px</div>
+        </div>
+      </div>
     </div>
   );
 };
