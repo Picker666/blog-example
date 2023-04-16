635f08cec82a89a6780238d533727affa6343ea0
Picker666 <1034790395@qq.com>
2023-04-16 19:00:52 +0800

    layout

--- src/component/Base/columnsLayout.tsx
+++ src/component/Base/columnsLayout.tsx
@@ -1,4 +1,4 @@
-import "./columnsLayout.less";
+import './columnsLayout.less';
 
 const ColumnsLayout = () => {
   return (
@@ -11,23 +11,43 @@ const ColumnsLayout = () => {
       </div>
 
       {/* 三栏 */}
-      {/* <div className="wrap">
+      <div className="wrap1">
         <div className="left">左侧</div>
         <div className="right">右侧</div>
         <div className="middle">中间</div>
-      </div> */}
-
-      {/* <div className="left">左边固定宽度</div>
-      <div className="main-wrapper">
-        <div className="main">中间自适应</div>
       </div>
-      <div className="right">右边固定宽度</div> */}
 
-      {/* <div className="wrap">
+      <div className="wrap2">
+        <div className="left">左边固定宽度</div>
+        <div className="main-wrapper">
+          <div className="main">中间自适应</div>
+        </div>
+        <div className="right">右边固定宽度</div>
+      </div>
+
+      <div className="wrap3">
         <div className="left">左侧</div>
         <div className="middle">中间</div>
         <div className="right">右侧</div>
-      </div> */}
+      </div>
+
+      <div className="wrap4">
+        <div className="left">左侧4</div>
+        <div className="middle">中间 宽度不支持个性化</div>
+        <div className="right">右侧</div>
+      </div>
+
+      <div className="wrap5">
+        <div className="left">左侧5</div>
+        <div className="middle">中间</div>
+        <div className="right">右侧</div>
+      </div>
+
+      <div className="wrap6">
+        <div className="left">左侧5</div>
+        <div className="middle">中间</div>
+        <div className="right">右侧</div>
+      </div>
     </div>
   );
 };
